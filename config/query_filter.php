01b2acb69128460abdd795ddffca1cd656586953
gene <gene@js-tech.tw>
2018-04-24 17:22:29 +0800

    update config content

--- config/query_filter.php
+++ config/query_filter.php
@@ -7,6 +7,13 @@ return [
     |--------------------------------------------------------------------------
     | @https://github.com/cerbero90/query-filters
     | Directory to store the query filters in. By default is app/QueryFilters.
+    |
+    | If set 'path' => 'QueryFilters',
+    | it will make a folder at
+    |
+    |   /project/app/QueryFilters/MyFilter.php
+    |
+    | when use command to make file 'php artisan make:query-filter MyFilter'
     */
     'path' => 'QueryFilters',
 ];
